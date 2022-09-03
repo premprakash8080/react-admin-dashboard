2905ed192414d1b41cc7094f4cb8630db072c2ac
test <premprakashyadav.stud@vivacollege.org>
2022-09-03 22:35:20 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {TooltipComponent} from '@syncfusion/ej2-react-popups';
 import './App.css';
 
 function App() {
+  const activeMenu  = true;
   return (
     <div>
       <BrowserRouter>
@@ -18,6 +19,28 @@ function App() {
             </button>
           </TooltipComponent>
           </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+              Sidebar 
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              Sidebar 
+            </div>
+          )}
+          <div
+            className={
+              activeMenu
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+            }
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              Navbar 
+            </div>
+            <div>
+          </div>
+      </div>
       </div>
       </BrowserRouter>
     </div>
